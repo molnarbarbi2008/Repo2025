3c2a00903e99315e94df6650595427cc8e459f7f
molnarbarbi2008 <barbi7660@gmail.com>
2025-10-07 12:23:10 +0200

    fajlbeolv

--- fajlbeolvasas.py
+++ fajlbeolvasas.py
@@ -1,28 +1,58 @@
 
 
 
+
+
+
 #A hibakezelés használata után a hiba üzenet megakadályozva lesz
 try:
     
     with open("F1-2024dec.csv" ,encoding="utf-8") as fajl:
-        f=fajl.read()
+        verseny_adatok=fajl.readlines()
         
-        n = 4 / 0
+
         
-        print(f)
-        print(n)
         
+except Exception as ex:
+    print(f"Halihóóóóóóóóó!: Hiba oka: {ex}") 
+    
 except FileNotFoundError:
     print("Hiba a fájl megnyitása közben!")
 
-except ZeroDivisionError:
-    print("Ne ossz nullával!")
+
+
+    """
+    1.[] Megszámolás
+    2.[] Eldöntés 1/ Eldöntés 2
+    3.[] Kiválasztás
+    4.[] Keresés
+    5.[] Sorozatszámítás
+    6.[] Minimum/maximum kiválasztás
+    7.[] Másolás
+    8.[] Kiválogatás
+    9.[]Szétválogatás
+    10.[]Metszet
+    11.[]Únió
     
-except Exception:
-    print("Halihóóóóóóóóó") 
+    12. Rendezés
+        [] Egyszerű cserés rendezés
+        [] Buborékos
+        [] Minimum kiválasztás
     
+    
+    
+    """
+
+
+    
+#------------------------------------------
+#1. Hány versenyző nem szerzett még pontot?
+db=0
+
+for i in range(1, len(verseny_adatok)):
+    if(int(verseny_adatok[i].split(',')[1])==0):
+        db=db+1
+        
+print(db, "versenyző nem szerzett még pontot!")
 
        
-print("ITT A PROGRAM VÉGE")
-
-
